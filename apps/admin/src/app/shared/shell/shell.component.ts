50c9277c27b45a23cee0a7c1fb4bd5985b1c1177
vinod kumar <vk.vinodkumar1215@gmail.com>
2025-01-18 18:20:37 +0530

    common styles folder structure added and started admin app

---
+++ apps/admin/src/app/shared/shell/shell.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'admin-shell',
+  templateUrl: './shell.component.html',
+  styleUrls: ['./shell.component.scss']
+})
+export class ShellComponent {
+
+}
--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
@@ -8,16 +9,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { UiModule } from '@bluebits/ui';
+import { AccordionModule } from 'primeng/accordion';
 
-const routes : Routes = [
-  {path: '', component: HomePageComponent},
-  {path: 'products', component: ProductListComponent}
-]
+const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'products', component: ProductListComponent }
+];
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes), UiModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
+    imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot(routes), UiModule, AccordionModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
